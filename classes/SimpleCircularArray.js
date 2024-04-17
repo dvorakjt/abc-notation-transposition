d85811052b86e25925731efd1e53213aa3442ee9
Joseph Dvorak <dvorakjt@gmail.com>
2024-04-17 04:07:01 -0400

    improved SimpleCircularArray negative index resolution algorithm, fixed vulnerabilities

--- classes/SimpleCircularArray.js
+++ classes/SimpleCircularArray.js
@@ -1,40 +1,40 @@
 class SimpleCircularArray {
-    _array;
+  _array;
 
-    constructor(array = []) {
-        this._array = array;
-    }
+  constructor(array = []) {
+    this._array = array;
+  }
 
-    get(index)  {
-        if(this._array.length === 0) throw new Error("Cannot access element of array length 0.");
-        //if we are moving through the array multiple times, apply % function so that only one
-        //traversal of the array is needed
-        if(index >= this._array.length) {
-            index = index % this._array.length;
-        }
-        while(index < 0) {
-            index += this._array.length;
-        }
-        return this._array[index];
-    }
+  get(index) {
+    if (this._array.length === 0)
+      throw new Error("Cannot access element of array length 0.");
 
-    size() {
-        return this._array.length;
+    if (index < 0) {
+      index = (index % this._array.length) + this._array.length;
     }
+    if (index >= this._array.length) {
+      index = index % this._array.length;
+    }
+    return this._array[index];
+  }
 
-    findIndex(testingFunction) {
-        for(let i = 0; i < this._array.length; i++) {
-            if(testingFunction(this._array[i])) return i;
-        }
-        return -1;
-    }
+  size() {
+    return this._array.length;
+  }
 
-    find(testingFunction) {
-        for(let elem of this._array) {
-            if(testingFunction(elem)) return elem;
-        }
-        return undefined;
+  findIndex(testingFunction) {
+    for (let i = 0; i < this._array.length; i++) {
+      if (testingFunction(this._array[i])) return i;
     }
+    return -1;
+  }
+
+  find(testingFunction) {
+    for (let elem of this._array) {
+      if (testingFunction(elem)) return elem;
+    }
+    return undefined;
+  }
 }
 
 module.exports.SimpleCircularArray = SimpleCircularArray;
